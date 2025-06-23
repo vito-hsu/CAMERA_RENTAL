33f4c1d761c84e122a04c7660e5060552365745b
Vito Hsu <garraypierce@gmail.com>
2025-06-23 08:43:41 +0800

    20250623

--- camera_rental/settings.py
+++ camera_rental/settings.py
@@ -126,9 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [        # add static files directory
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = [BASE_DIR / 'static',]
+STATIC_ROOT = BASE_DIR / 'staticfiles' # <-- 新增這一行！
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
